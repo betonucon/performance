d2e1e47df8a5bfda287c62a32caf3b936f7a76f0
projectucon <project101290@gmail.com>
2020-07-08 08:26:07 +0700

    login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,14 +26,36 @@ class LoginController extends Controller
      * @var string
      */
 
-    protected function credentials(\Illuminate\Http\Request $request)
+    // protected function credentials(\Illuminate\Http\Request $request)
+    // {
+    //     if(is_numeric($request->get('email'))){
+    //         return ['nik'=>$request->get('email'),'password'=>$request->get('password')];
+    //     }else{
+    //         return ['nik' => $request->{$this->username()}, 'password' => $request->password];
+    //     }
+    //     return $request->only($this->username(), 'password');
+    // }
+    public function programaticallyEmployeeLogin(Request $request, $personnel_no)
     {
-        if(is_numeric($request->get('email'))){
-            return ['nik'=>$request->get('email'),'password'=>$request->get('password')];
+        $personnel_no = base64_decode($personnel_no);
+        try {
+        
+        $userlogin = User::where('nik', $personnel_no)->first();
+        //dd($userlogin);
+        if(is_null($userlogin)){
+            return redirect('https://sso.krakatausteel.com');
         }else{
-            return ['nik' => $request->{$this->username()}, 'password' => $request->password];
+            Auth::loginUsingId($userlogin->id);
+            return redirect()
+            ->route('home');
         }
-        return $request->only($this->username(), 'password');
+        
+        } catch (ModelNotFoundException $e) {
+    
+        return redirect('https://sso.krakatausteel.com');
+        }
+
+        return $this->sendLoginResponse($request);
     }
     protected $redirectTo = '/home';
 

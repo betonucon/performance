c8152e47fc9faba114670772e75bad47290576cb
projectucon <project101290@gmail.com>
2020-07-10 11:11:02 +0700

    manda

--- app/Imports/TargetImport.php
+++ app/Imports/TargetImport.php
@@ -17,28 +17,63 @@ class TargetImport implements ToModel, WithStartRow
     public function model(array $row)
     {
         // $cek=Deployment::where('kode_unit',$row['0'])->where('kode_kpi',$row['2'])->first();
-        $deploy             =Deployment::where('kode_unit',$row['0'])->where('kode_kpi',$row['1'])->where('tahun',$row['2'])->first();
-        $deploy->sts        = 1;
-        $deploy->save();
+        if($row['0']=='all'){
+            $get_deploy             =Deployment::where('kode_kpi',$row['1'])->where('tahun',$row['2'])->get();
+            
+            foreach($get_deploy as $get){
+                $deploy             =Deployment::where('id',$get['id'])->first();
+                $deploy->sts        = 1;
+                $deploy->save();
 
-        if($deploy){
-            
-            $cektarget =Target::where('deployment_id',$deploy['id'])->where('bulan',$row[3])->count();
-            if($cektarget>0){
-                $target             =Target::where('deployment_id',$deploy['id'])->where('bulan',$row[3])->first();
-                $target->target     = $row[4];
-                $target->realisasi     = $row[5];
-                $target->save();
-            }else{
-                $target             = new Target;
-                $target->deployment_id     = $deploy['id'];
-                $target->bulan     = $row[3];
-                $target->target     = $row[4];
-                $target->realisasi     = $row[5];
-                $target->status_realisasi     = 1;
-                $target->save();
+                if($deploy){
+                    
+                    $cektarget =Target::where('deployment_id',$deploy['id'])->where('bulan',$row[3])->count();
+                    if($cektarget>0){
+                        $target             =Target::where('deployment_id',$deploy['id'])->where('bulan',$row[3])->first();
+                        $target->target     = $row[4];
+                        $target->realisasi     = $row[5];
+                        $target->save();
+                    }else{
+                        $target             = new Target;
+                        $target->deployment_id     = $deploy['id'];
+                        $target->bulan     = $row[3];
+                        $target->target     = $row[4];
+                        $target->realisasi     = $row[5];
+                        $target->status_realisasi     = 1;
+                        $target->save();
+                    }
+                    
+                }
             }
-            
+
+
+        }
+        
+        
+        else{
+            $deploy             =Deployment::where('kode_unit',$row['0'])->where('kode_kpi',$row['1'])->where('tahun',$row['2'])->first();
+            $deploy->sts        = 1;
+            $deploy->save();
+
+            if($deploy){
+                
+                $cektarget =Target::where('deployment_id',$deploy['id'])->where('bulan',$row[3])->count();
+                if($cektarget>0){
+                    $target             =Target::where('deployment_id',$deploy['id'])->where('bulan',$row[3])->first();
+                    $target->target     = $row[4];
+                    $target->realisasi     = $row[5];
+                    $target->save();
+                }else{
+                    $target             = new Target;
+                    $target->deployment_id     = $deploy['id'];
+                    $target->bulan     = $row[3];
+                    $target->target     = $row[4];
+                    $target->realisasi     = $row[5];
+                    $target->status_realisasi     = 1;
+                    $target->save();
+                }
+                
+            }
         }
             
     }

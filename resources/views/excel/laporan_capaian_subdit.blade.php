06f838e1b2e40b7700559ba79559fd3489af08f3
betonucon <betonucon@gmail.com>
2021-03-01 14:19:24 +0700

    perbaikan excel

--- resources/views/excel/laporan_capaian_subdit.blade.php
+++ resources/views/excel/laporan_capaian_subdit.blade.php
@@ -1,6 +1,6 @@
 <?php
-            // header("Content-type: application/vnd-ms-excel");
-            // header("Content-Disposition: attachment; filename=".str_replace(',','_',cek_unit($kode)['nama'])."-".$tahun.".xls");
+            header("Content-type: application/vnd-ms-excel");
+            header("Content-Disposition: attachment; filename=".str_replace(',','_',cek_unit($kode)['nama'])."-".$tahun.".xls");
         ?>
 <html>
     <head>  

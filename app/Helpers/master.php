08c4b4f96319060fb0494c19323de6db863539de
betonucon <betonucon@gmail.com>
2022-05-27 10:39:40 +0700

    perbaikan

--- app/Helpers/master.php
+++ app/Helpers/master.php
@@ -1255,7 +1255,12 @@ function akumulasi_capaian($id,$target=null,$realisasi=null){
                 if($target<0 && $realisasi>0){
                     $cap=1+(((($target)-($realisasi))/($target))*100);
                 }else{
-                    $cap=(1-((($target)-($realisasi))/($target)))*100;
+                    if($target<0 && $realisasi<0){
+                        $cap=(1+(($target)-($realisasi))/($target))*100;
+                    }else{
+                        $cap=(1-((($target)-($realisasi))/($target)))*100;
+                    }
+                    
                 }
                     
                 
